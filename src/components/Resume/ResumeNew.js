67dee643fa990fe95f5fb402c9e167253ea0be7f
HABIB UR REHMAN <habibazeem658@gmail.com>
2025-09-12 23:51:35 +0500

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Suspense, lazy } from "react";
-import React, { useState } from "react";
+
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
